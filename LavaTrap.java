af51a42972236bbba1732930c573bca6c9189de1
VitoKloots <302243137@student.rocmondriaan.nl>
2018-11-29 09:59:54 +0100

    Geen idee

---
+++ LavaTrap.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LavaTrap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LavaTrap extends Actor
+{
+    
+    public void Lava_trap() 
+    {
+        setImage("LavaRemove.png");
+    }    
+}
